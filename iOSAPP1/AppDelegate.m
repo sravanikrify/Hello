8656d7c78f9b33f7585d15b80b025f1c7ed3c370
Sravani <krishnareddykovvuri@Sravanis-Mac-mini.local>
2014-04-01 18:49:33 +0530

    add

--- iOSAPP1/AppDelegate.m
+++ iOSAPP1/AppDelegate.m
@@ -12,7 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    NSLog(@"sdfasd");
+    NSLog(@"sdfasdasd");
     return YES;
 }
 							
